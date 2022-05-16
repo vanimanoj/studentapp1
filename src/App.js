6a615aa8b3411034541ea836bd29dc525b59685a
vaniammu@123 <vanimanoj1997@gmail.com>
2022-05-16 09:58:12 +0530

    searchstudent

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addstudent from './component/Addstudent';
+import Searchstudent from './component/Searchstudent';
 
 function App() {
   return (
-    <Addstudent/>
+    <Searchstudent/>
   );
 }
 
