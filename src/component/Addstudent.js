44d09b5fdefe5987fd583d0cf14685bbb3699c72
vaniammu@123 <vanimanoj1997@gmail.com>
2022-05-16 09:27:21 +0530

    addstudentupdate

--- src/component/Addstudent.js
+++ src/component/Addstudent.js
@@ -2,7 +2,39 @@ import React from 'react'
 
 const Addstudent = () => {
   return (
-    <div>Addstudent</div>
+    <div>
+        <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label for="" className="form-label">Name</label>
+                <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">RollNo</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Admission No</label>
+                <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">University NO</label>
+                <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">College</label>
+                <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button className="btn btn-success">REGISTER</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
   )
 }
 
