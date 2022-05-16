b022c7eb34b12b34bab01a784f3a72b0cc3b1072
vaniammu@123 <vanimanoj1997@gmail.com>
2022-05-16 23:27:28 +0530

    navbar

--- src/component/Addstudent.js
+++ src/component/Addstudent.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navabar from './Navabar'
 
 const Addstudent = () => {
   return (
     <div>
+        <Navabar/>
         <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
